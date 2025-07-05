82fdf1bb0c2697b8969522aaf2be8aba5068e08c
Declan L. Scott <declanlscott@gmail.com>
2025-07-04 23:42:42 -0500

    chore: bump deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare namespace App {
-  interface Locals {
-    session: import("lucia").Session | null;
-    user: import("lucia").User | null;
-  }
+  interface Locals {}
 }
